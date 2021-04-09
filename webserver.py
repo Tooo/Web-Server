9ee60ac655758b6121be8fb47ca8d88c093ce341
Travis <34826963+Tooo@users.noreply.github.com>
2021-04-08 22:20:16 -0700

    Adding badrequest create response function

--- webserver.py
+++ webserver.py
@@ -4,20 +4,18 @@ import time
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8080
 
-server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.bind((SERVER_HOST, SERVER_PORT))
-server_socket.listen(1)
-print('Listening on port %s ...' % SERVER_PORT)
+def get_response(client_connection):
+    try:
+        start_time = time.time()
+        request = client_connection.recv(1048576).decode()
+        end_time = time.time()
+        print(request)
+        print(start_time, end_time, end_time-start_time)
+    except:
+        return 'HTTP/1.0 400 BAD REQUEST\n\nBad Request'
 
-while True:
-    client_connection, client_address = server_socket.accept()
-    start_time = time.time()
-    request = client_connection.recv(1048576).decode()
-    end_time = time.time()
-    # print(request)
-    print(start_time, end_time, end_time-start_time)
     if end_time - start_time > 1:
-        response = 'HTTP/1.0 408 REQUEST TIMED OUT\n\nRequest Timed Out' 
+        return 'HTTP/1.0 408 REQUEST TIMED OUT\n\nRequest Timed Out' 
     else:
         headers = request.split('\n')
         filename = headers[0].split()[1]
@@ -27,12 +25,21 @@ while True:
             fin = open("templates"+filename)
             content = fin.read()
             fin.close()
-            response = 'HTTP/1.0 200 OK\n\n' + content
+            return 'HTTP/1.0 200 OK\n\n' + content
         except FileNotFoundError:
-            response = 'HTTP/1.0 404 NOT FOUND\n\nFile Not Found'
-        
-    client_connection.sendall(response.encode())
+            return 'HTTP/1.0 404 NOT FOUND\n\nFile Not Found'
+        except:
+            return 'HTTP/1.0 400 BAD REQUEST\n\nBad Request'
 
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+server_socket.listen(1)
+print('Listening on port %s ...' % SERVER_PORT)
+
+while True:
+    client_connection, client_address = server_socket.accept()
+    response = get_response(client_connection)
+    client_connection.sendall(response.encode())
     client_connection.close()
 
 
